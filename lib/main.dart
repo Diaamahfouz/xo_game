e6d38eba5b5943ef0f58707d4ae0cf329a090464
Diaa Mahfouz <diaamahfouz229@gmail.com>
2024-09-07 17:31:28 +0300

    implement (X_O) game

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:xo_game/game_bord_screeen.dart';
+import 'package:xo_game/login_screen.dart';
+
+
+void main() {
+  runApp(const XOGame());
+}
+
+class XOGame extends StatelessWidget {
+  const XOGame({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        GameBoardScreen.routeName: (context) => const GameBoardScreen(),
+      },
+      initialRoute: LoginScreen.routeName,
+    );
+  }
+}
